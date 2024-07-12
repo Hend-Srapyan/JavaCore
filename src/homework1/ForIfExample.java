918d26c202cb826cfaa598a5577174a1ac8a033f
Henrik Srapyan <hensrap@gmail.com>
2024-07-12 13:56:20 +0400

    Matrix Example

--- src/homework1/ForIfExample.java
+++ src/homework1/ForIfExample.java
@@ -3,32 +3,31 @@ package homework1;
 public class ForIfExample {
     public static void main(String[] args) {
         int x;
-        for (x = 1; x < 11; x++) {
-            System.out.println(x);
+        for (x = 0; x <= 10; x++) {
+            System.out.print(x + " ");
         }
-        System.out.println();
-        int b = 0;
-        for (x = 0; x <= 100; x++) {
-            b = b +x;
-            System.out.println(x);
-        }
-        System.out.println("Գումարը = " + b);
         System.out.println();
 
+        int b = 0;
+        for (x = 0; x <= 100; x++) {
+            b = b + x;
+        }
+        System.out.println(b);
+
         for (x = 50; x >= 0; x--) {
-            System.out.println(x);
+            System.out.print(x + " ");
         }
         System.out.println();
 
         for (x = 1; x < 20; x += 2) {
-            System.out.println(x);
+            System.out.print(x + " ");
         }
         System.out.println();
+
         int y = 0;
-        for (x = 0; x <= 100; x += 2) {
-            y = y + 1;
-            System.out.println(x);
+        for (x = 2; x <= 100; x += 2) {
+            y++;
         }
-        System.out.println("Ընդհանուր քանակը = " + y);
+        System.out.println(y);
     }
 }
