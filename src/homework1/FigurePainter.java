035937235b66bf8a29e9aafe64834f18cce3a026
Henrik Srapyan <hensrap@gmail.com>
2024-07-15 16:08:39 +0400

    Home Work 2

--- src/homework1/FigurePainter.java
+++ src/homework1/FigurePainter.java
@@ -3,25 +3,28 @@ package homework1;
 
 public class FigurePainter {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
+        System.out.println("1)");
+        for (int i = 0; i <= 5; i++) {
             for (int j = 0; j < i; j++) {
                 System.out.print("* ");
             }
-            System.out.println("* ");
+            System.out.println();
         }
         System.out.println();
         System.out.println();
 
-        for (int i = 4; i >= 0; i--) {
+        System.out.println("2)");
+        for (int i = 5; i >= 0; i--) {
             for (int j = i; j > 0; j--) {
                 System.out.print("* ");
             }
-            System.out.println("* ");
+            System.out.println();
 
         }
         System.out.println();
         System.out.println();
 
+        System.out.println("3)");
         int x = 5;
         for (int i = 0; i < x; i++) {
             for (int j = 0; j < x - i - 1; j++) {
@@ -36,7 +39,7 @@ public class FigurePainter {
         System.out.println();
         System.out.println();
 
-
+        System.out.println("4)");
         for (int i = 6; i > 0; i--) {
             for (int j = 6 - i; j > 0; j--) {
                 System.out.print("  ");
@@ -51,7 +54,7 @@ public class FigurePainter {
         System.out.println();
         System.out.println();
 
-
+        System.out.println("5)");
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5 - i - 1; j++) {
                 System.out.print(" ");
